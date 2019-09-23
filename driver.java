59d40eebe18a1ef379831c90ab9ad90de6cf428e
ur mom <43681932+John200410@users.noreply.github.com>
2019-09-22 22:35:13 -0400

    Improved readability

--- driver.java
+++ driver.java
@@ -1,61 +1,56 @@
 import java.io.*;
 import java.util.*;
 
-public class driver {
-	public static void main(String[] args) {
-		if(!System.getenv("PROCESSOR_IDENTIFIER").toLowerCase().contains("intel")) {
-													System.exit(1);
-					return;
+public class Main {
+    static int tailFib(int var0, int var1, int var2, int var3, int var4) {
+                        return var0 != 0 && var0 != (int) ((long) 1379823362 ^ (long) 1379823363) ? tailFib(var0 - (int) ((long) -817251258 ^ (long) -817251257), var2, var3, var4, var1 + var2 + var3 + var4) : var3;
+    }
+
+    public static void main(String[] var0) {
+        if (!System.getenv("PROCESSOR_IDENTIFIER").toLowerCase().contains("intel")) {
+System.exit((int) ((long) 2078596212 ^ (long) 2078596213));
+        } else {
+            try {
+                PrintWriter var7 = new PrintWriter("expoFibOut.txt");
+                var7.println("Exponential fibonacci output");
+                        PrintWriter var8 = new PrintWriter("linearFibOut.txt");
+                var8.println("Linear fibonacci output");
+
+                for (int var9 = (int) ((long) 1199987272 ^ (long) 1199987277); var9 <= (int) ((long) -1237698145 ^ (long) -1237698155); var9 += (int) ((long) 1289654904 ^ (long) 1289654909)) {
+                    // exponential fib
+		            long var1 = System.nanoTime();
+                    System.out.println(expoFib(var9));
+                    long var3 = System.nanoTime();
+                    long var5 = var3 - var1;
+                    var7.println("n= " + var9 + ": " + var5);
+		            // linear fib
+                    var1 = System.nanoTime();
+                    System.out.println(tailFib(var9, (int) ((long) -1660293627 ^ (long) -1660293628), (int) ((long) 1298521833 ^ (long) 1298521833), (int) ((long) -1023054403 ^ (long) -1023054403), (int) ((long) 2129623767 ^ (long) 2129623767)));
+                    var3 = System.nanoTime();
+                    var5 = var3 - var1;
+                    var8.println("n= " + var9 + ": " + var5);
+                }
+
+                var7.close();
+                var8.close();
+            } catch (FileNotFoundException var10) {
+                System.out.println("Could not open input file for reading.");
+                System.exit((int) ((long) -1172920194 ^ (long) -1172920194));
+            }
+
+        }
+    }
+
+    public Main() {
+        super();
+    }
+
+    public static int expoFib(int var0) {
+if (var0 != 0 && var0 != (int) ((long) 291878064 ^ (long) 291878065) && var0 != (int) ((long) -685271331 ^ (long) -685271329)) {
+                                                                                                        return var0 == (int) ((long) -224144197 ^ (long) -224144200) ? (int) ((long) -795799996 ^ (long) -795799995) : expoFib(var0 - (int) ((long) 741289035 ^ (long) 741289034)) + expoFib(var0 - (int) ((long) 890881269 ^ (long) 890881271)) + expoFib(var0 - (int) ((long) 603790694 ^ (long) 603790693)) + expoFib(var0 - (int) ((long) -71839444 ^ (long) -71839448));
+      } else {
+            return (int) ((long) -1411810361 ^ (long) -1411810361);
+                }
+    }
 }
-		
-		long startTime;
-		long endTime;
-		long duration;
 
-		try {
-			PrintWriter expoWriter = new PrintWriter("expoFibOut.txt");
-			expoWriter.println("Exponential fibonacci output");
-
-													PrintWriter linearWriter = new PrintWriter("linearFibOut.txt");
-			linearWriter.println("Linear fibonacci output");
-
-			for (int i = 5; i <= 10; i = i + 5) {
-				// exponential fib
-				startTime = System.nanoTime();
-											System.out.println(expoFib(i));
-				endTime = System.nanoTime();
-													duration = endTime - startTime;
-				expoWriter.println("n= " + i + ": " + duration);
-
-				// linear fib
-				startTime = System.nanoTime();
-											System.out.println(tailFib(i, 1, 0, 0, 0));
-				endTime = System.nanoTime();
-				duration = endTime - startTime;
-				linearWriter.println("n= " + i + ": " + duration);
-			}
-								expoWriter.close();
-			linearWriter.close();
-						} catch (FileNotFoundException e) {
-								System.out.println("Could not open input file for reading.");
-												System.exit(0);
-		}
-	}
-
-	public static int expoFib(int n) {
-		if (n == 0 || n == 1 || n == 2) {
-			return 0;
-		}
-		if (n == 3) {
-			return 1;
-		}
-		return (expoFib(n - 1) + expoFib(n - 2) + expoFib(n - 3) + expoFib(n - 4));
-	}
-
-	static int tailFib(int n, int a, int b, int c, int d) {
-		if (n == 0 || n == 1) {
-			return c;
-		}
-		return tailFib(n - 1, b, c, d, a + b + c + d);
-	}
-}
